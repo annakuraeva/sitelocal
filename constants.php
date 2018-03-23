fe97bb311111bcb4fe5fb16de4da3ac5418b1cd4
annakuraeva <37697404+annakuraeva@users.noreply.github.com>
2018-03-23 18:39:54 +0300

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database Constants
+define("DB_SERVER", "localhost");
+define("DB_USER", "admin");
+define("DB_PASS", "nimda");
+define("DB_NAME", "userlistdb");
+
+?>
---
+++ userlistdb.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 23 2018 г., 13:22
+-- Версия сервера: 10.1.31-MariaDB
+-- Версия PHP: 5.6.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `userlistdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `usertbl`
+--
+
+CREATE TABLE `usertbl` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `usertbl`
+--
+
+INSERT INTO `usertbl` (`id`, `full_name`, `email`, `username`, `password`) VALUES
+(4, 'Juliya Park', 'parkjul@wer.ru', 'ParkJuli', 'krap'),
+(3, 'Nikolay Lvov', 'Lvovnik@wer.ru', 'Nikolay', 'yalokin'),
+(5, 'Win Gin', 'wingin@wer.ru', 'WinGin007', '700nigniw'),
+(6, 'Karina Popova', 'poposha@wer.ru', 'Karina_meow', 'woem_anirak'),
+(7, 'Marina Rostova', 'rostmar@wer.ru', 'Marina666', '666aniram'),
+(8, 'Petr Parker', 'parkerpet@wer.ru', 'petrparker1', '1rekraprtep');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `usertbl`
+--
+ALTER TABLE `usertbl`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `usertbl`
+--
+ALTER TABLE `usertbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
